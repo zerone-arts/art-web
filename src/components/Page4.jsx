28b4417f5fec6d399c63c7c3bd973f000db647eb
zerone <zerone.dot5@gmail.com>
2023-12-14 19:43:51 +0900

    page4 slide clear

--- src/components/Page4.jsx
+++ src/components/Page4.jsx
@@ -1,33 +1,68 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import "../assets/css/Page4.css";
-const page4List = [
-  { no: 1, title: "Photography" },
-  { no: 2, title: "Illustration" },
-  { no: 3, title: "Photography" },
-  { no: 4, title: "Illustration" },
-  { no: 6, title: "Photography" },
-  { no: 7, title: "Illustration" },
-  { no: 8, title: "Illustration" },
-  { no: 9, title: "Illustration" },
-  { no: 10, title: "Illustration" },
-];
-// const page4Lists = [...page4List];
+import { DownUseInterval } from "../assets/utils";
 
-// page4Lists.unshift(...page4List);
 function Page4(props) {
+  const slideUlRef = useRef(null);
+  const [isPlay, setIsPlay] = useState(true);
+  const [playHover, setPlayHover] = useState(false);
+  const [px, setPx] = useState(0);
+
+  console.log(px);
+  let ulWidth = slideUlRef.current?.clientWidth;
+  console.log(ulWidth);
+
+  DownUseInterval(
+    () => {
+      setPx(px + 1);
+      if (px === ulWidth) {
+        setIsPlay(false);
+      }
+    },
+    isPlay ? 10 : null
+  );
+
+  useEffect(() => {
+    if (!isPlay && !playHover) {
+      setIsPlay(true);
+      setPx(0);
+    }
+  }, [isPlay]);
+  useEffect(() => {
+    if (playHover) {
+      setIsPlay(false);
+    } else {
+      setIsPlay(true);
+    }
+  }, [playHover]);
+
   return (
     <div className="p4-container">
       <div className="p4-title">Art Magazine</div>
-      <div className="p4-slide-Wrapper">
-        <ul>
-          {page4List.map((item, idx) => {
-            return <li key={idx}>{item.no}</li>;
-          })}
-        </ul>
-        <ul>
-          {page4List.map((item, idx) => {
-            return <li key={idx}>{item.no}</li>;
-          })}
+      <div
+        className="p4-slide-Wrapper"
+        onMouseOver={() => setPlayHover(true)}
+        onMouseLeave={() => {
+          setPlayHover(false);
+        }}
+      >
+        <ul
+          className="p4-slideBox"
+          ref={slideUlRef}
+          style={{ transform: `translateX(-${px}px)` }}
+        >
+          <li className="page4-object1">
+            <ul className="page4-objectImgWrapper">
+              <li className="page-object1-left">왼쪽</li>
+              <li className="page-object1-center">중간</li>
+              <li className="page-object1-right">오른쪽</li>
+            </ul>
+          </li>
+          <li className="page4-object2"></li>
+          <li className="page4-object3"></li>
+          <li className="page4-object4"></li>
+          <li className="page4-object5"></li>
+          <li className="page4-object6"></li>
         </ul>
       </div>
     </div>
