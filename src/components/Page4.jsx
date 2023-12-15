28aa3f07f89211e20e2b65d9b2205792183c8836
zerone <zerone.dot5@gmail.com>
2023-12-15 19:42:21 +0900

    page4 object1,2 clear

--- src/components/Page4.jsx
+++ src/components/Page4.jsx
@@ -1,16 +1,17 @@
 import React, { useState, useEffect, useRef } from "react";
 import "../assets/css/Page4.css";
 import { DownUseInterval } from "../assets/utils";
-
+import page4Object1Img from "../assets/img/page4-object1-Img.jpeg";
+import page4Object1Sign from "../assets/img/page4-object1-sign.png";
+import page4Object2Img11 from "../assets/img/page4-object2-1.png";
+import page4Object2Img12 from "../assets/img/page4-object2-2.png";
 function Page4(props) {
   const slideUlRef = useRef(null);
   const [isPlay, setIsPlay] = useState(true);
   const [playHover, setPlayHover] = useState(false);
   const [px, setPx] = useState(0);
 
-  console.log(px);
   let ulWidth = slideUlRef.current?.clientWidth;
-  console.log(ulWidth);
 
   DownUseInterval(
     () => {
@@ -49,16 +50,73 @@ function Page4(props) {
         <ul
           className="p4-slideBox"
           ref={slideUlRef}
-          style={{ transform: `translateX(-${px}px)` }}
+          // style={{ transform: `translateX(-${px}px)` }}
+          style={{ transform: `translateX(20px)` }}
         >
           <li className="page4-object1">
-            <ul className="page4-objectImgWrapper">
-              <li className="page-object1-left">왼쪽</li>
-              <li className="page-object1-center">중간</li>
-              <li className="page-object1-right">오른쪽</li>
+            <div className="page4-object1-title">
+              永<br />日
+            </div>
+            <ul className="page4-object1-ImgWrapper">
+              <li className="page-object1-left">
+                <img src={page4Object1Img} alt="page4Object1Img" />
+              </li>
+              <li className="page-object1-center">
+                <img src={page4Object1Img} alt="page4Object1Img" />
+              </li>
+              <li className="page-object1-right">
+                <img src={page4Object1Img} alt="page4Object1Img" />
+              </li>
             </ul>
+            <div className="page4-object1-line"></div>
+            <img
+              className="page4-object1-sign"
+              src={page4Object1Sign}
+              alt="page4Object1Sign"
+            />
+            <div className="page4-object1-text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor,
+            </div>
           </li>
-          <li className="page4-object2"></li>
+          <li className="page4-object2">
+            <img src={page4Object2Img11} alt="page4Object2Img11" />
+            <img
+              className="page4-object2-img"
+              src={page4Object2Img12}
+              alt="page4Object2Img12"
+            />
+            <div className="page4-object2-textBox">
+              <div className="page4-object2-textBox-1">
+                <p>
+                  De<span></span>
+                </p>
+                <p>Vision</p>
+                <p>010</p>
+              </div>
+              <div className="page4-object2-textBox-2">23 - 12 - 15</div>
+              <div className="page4-object2-textBox-3">
+                <h2>
+                  Kim <br />
+                  Chunsik
+                </h2>
+                <p>Artist</p>
+                <p>Just write something down</p>
+              </div>
+              <ul className="page4-object2-textBox-4">
+                <li>Z</li>
+                <li>e</li>
+                <li>r</li>
+                <li>O</li>
+                <li>n</li>
+                <li>e</li>
+              </ul>
+              <div className="page4-object2-textBox-5">
+                <p>From 19 30 PM</p>
+                <p>Third week Friday</p>
+                <p>Lorem ipsum sit consectetur.</p>
+              </div>
+            </div>
+          </li>
           <li className="page4-object3"></li>
           <li className="page4-object4"></li>
           <li className="page4-object5"></li>
