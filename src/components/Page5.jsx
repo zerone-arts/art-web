c8cc0e9491765f03a35661fefa7b800bbad39bb1
zerone <zerone.dot5@gmail.com>
2023-12-26 18:56:44 +0900

    page5 object1 100% , page5 object2 1%

--- src/components/Page5.jsx
+++ src/components/Page5.jsx
@@ -3,16 +3,23 @@ import "../assets/css/Page5.css";
 import page5Object1Img from "../assets/img/p5-object1-img.png";
 function Page5(props) {
   const [page5Start, setPage5Start] = useState("");
+  const [page5Object2Start, setPage5Object2Start] = useState("");
   const page5Ref = useRef(null);
+  const page5Object2Ref = useRef(null);
 
   const menuBar = new Array(9).fill("");
-  console.log(menuBar);
 
   const onScroll = () => {
     if (page5Ref.current?.getBoundingClientRect().top > 0) {
       setPage5Start("");
     } else {
       setPage5Start("active");
+    }
+
+    if (page5Object2Ref.current?.getBoundingClientRect().top - 200 > 0) {
+      setPage5Object2Start("");
+    } else {
+      setPage5Object2Start("page2Active");
     }
   };
 
@@ -24,7 +31,10 @@ function Page5(props) {
   }, []);
 
   return (
-    <div className={`p5-container ${page5Start}`} ref={page5Ref}>
+    <div
+      className={`p5-container ${page5Start} ${page5Object2Start}`}
+      ref={page5Ref}
+    >
       <div className={`p5-bgLight ${page5Start}`}></div>
       <div className="p5-object1">
         <div className={`p5-object1-circle ${page5Start}`}></div>
@@ -59,13 +69,16 @@ function Page5(props) {
           </h2>
         </div>
         <div className={`p5-object1-text ${page5Start}`}>
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-            Necessitatibus enim maxime quod molestias perspiciatis, possimus
-            laborum ex nisi sed hic!
-          </p>
+          <p>Lorem ipsum dolor sit,</p>
+          <p>amet consectetur.</p>
+          <p> Necessitatibus enim </p>
+          <p>maxime quod molestias.</p>
         </div>
       </div>
+      <div
+        className={`p5-object2 ${page5Object2Start}`}
+        ref={page5Object2Ref}
+      ></div>
     </div>
   );
 }
